69349a54aa58ff43f3534a0358b5a947f8af021b
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-25 17:48:16 -0300

    feat: created ActivityRepo

---
+++ src/main/java/extension/extension/activity/utils/ActivityRepository.java
@@ -0,0 +1,6 @@
+package extension.extension.activity.utils;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<Activity, String> {
+}
