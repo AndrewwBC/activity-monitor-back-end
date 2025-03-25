0536e8978bc77102651be404129e8bf90065f36b
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-25 18:06:58 -0300

    fix userDetails

--- src/main/java/extension/extension/activity/Activity.java
+++ src/main/java/extension/extension/activity/Activity.java
@@ -15,7 +15,7 @@ import java.util.Collection;
 @Table(name = "activity")
 @AllArgsConstructor
 @NoArgsConstructor
-public class Activity implements UserDetails {
+public class Activity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
@@ -36,40 +36,5 @@ public class Activity implements UserDetails {
                 ", creationDate='" + creationDate + '\'' +
                 ", endDate='" + endDate + '\'' +
                 '}';
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
     }
 }
--- src/main/java/extension/extension/user/User.java
+++ src/main/java/extension/extension/user/User.java
@@ -4,14 +4,17 @@ import extension.extension.activity.Activity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity(name = "Users")
 @Table(name = "users")
 @Setter
 @Getter
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
@@ -36,5 +39,30 @@ public class User {
                 ", password='" + password + '\'' +
                 ", activities=" + activities +
                 '}';
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
