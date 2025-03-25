abe549cb914bee7cd0780af0df4dd84db5db1f6b
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-25 17:47:12 -0300

    add(User): implemented UserDetails

--- src/main/java/extension/extension/activity/utils/Activity.java
+++ src/main/java/extension/extension/activity/utils/Activity.java
@@ -3,10 +3,14 @@ package extension.extension.activity.utils;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import extension.extension.user.User;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Table(name = "activity")
-public class Activity {
+public class Activity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
@@ -27,5 +31,40 @@ public class Activity {
                 ", creationDate='" + creationDate + '\'' +
                 ", endDate='" + endDate + '\'' +
                 '}';
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
