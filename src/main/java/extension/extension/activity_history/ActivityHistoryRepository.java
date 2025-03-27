17f64e6a034ff94f467fe90296931f449db63d78
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-26 21:17:44 -0300

    add: create activity repository

---
+++ src/main/java/extension/extension/activity_history/ActivityHistoryRepository.java
@@ -0,0 +1,6 @@
+package extension.extension.activity_history;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityHistoryRepository extends JpaRepository<ActivityHistory, String> {
+}
